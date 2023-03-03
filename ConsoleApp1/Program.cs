41e33271c320615020a97902098b4f5dd9f2417f
ThisIsMe32 <43524319+ThisIsMe32@users.noreply.github.com>
2023-03-03 14:35:53 +0100

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+var client = new HttpClient();
+var sequence = File.ReadAllText("sequence.txt");
+var signatureBase64 = File.ReadAllText("signature.txt.base64");
+var signatureBytes = Convert.FromBase64String(signatureBase64);
+var content = new MultipartFormDataContent();
+content.Add(new StringContent(sequence));
+content.Add(new ByteArrayContent(signatureBytes), "signature", "signature.bin");
+var response = await client.PostAsync("https://staging-a-bon.aircash.eu/rest/api/CashRegister/CreateCoupon", content);
+
+if (response.IsSuccessStatusCode)
+{
+    Console.WriteLine("Succesful post");
+}
+else
+{
+    Console.WriteLine($"Failed post\n {response.StatusCode}");
+}
+Console.WriteLine(response);
+Console.ReadLine();
